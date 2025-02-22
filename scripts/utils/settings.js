07978cb7cda01663f9e3780ab132114b6a1127f4
R <R>
2025-02-22 21:26:47 +0000

    Update setting descriptions

--- scripts/utils/settings.js
+++ scripts/utils/settings.js
@@ -4,6 +4,7 @@ export function setupSettings() {
     // game.settings.get("helianas-harvesting", "playerRecipes"); // returns true
     game.settings.register("helianas-harvesting", "playerRecipes", {
         name: "HelianasHarvest.SettingsPlayerRecipes",
+        hint: "HelianasHarvest.SettingsPlayerRecipesHint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.BooleanField(),
@@ -12,6 +13,7 @@ export function setupSettings() {
 
     game.settings.register("helianas-harvesting", "playerCrafting", {
         name: "HelianasHarvest.SettingsPlayerCrafting",
+        hint: "HelianasHarvest.SettingsPlayerCraftingHint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.BooleanField(),
@@ -20,6 +22,7 @@ export function setupSettings() {
 
     game.settings.register("helianas-harvesting", "playerHarvesting", {
         name: "HelianasHarvest.SettingsPlayerHarvesting",
+        hint: "HelianasHarvest.SettingsPlayerHarvestingHint",
         scope: "world",
         config: true,
         type: new foundry.data.fields.BooleanField(),
