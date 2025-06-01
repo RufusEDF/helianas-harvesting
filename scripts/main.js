20a7630164479a274b61b99cfd9c58cb6591501c
R <R>
2025-06-01 09:03:28 +0100

    remove unused import

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 import { bindStatisticsButton } from "./utils/bindStatisticsButton.js";
-import { loadModules } from "./utils/loadModules.js";
 import { bindSceneControlButtons } from "./utils/bindSceneControlButtons.js";
 import { initializeDatabases } from "./utils/initializeDatabases.js";
 import { setupModuleAPI } from "./utils/setupModuleAPI.js";
